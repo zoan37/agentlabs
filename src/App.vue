645ba9c5d57d4d743f7af03caaec816f7f18a249
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-21 23:05:53 -0400

    vrm

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+// @ts-ignore
+import { startGame } from "./game.js";
+
+onMounted(() => {
+  startGame();
+});
+</script>
+
+<template>
+  <div id="divInfo">
+    drag and drop <a href="https://www.mixamo.com/" target="_blank" rel="noopener noreferrer">mixamo</a> animation
+    (.fbx)<br />
+  </div>
+</template>
+
+<style scoped>
+#divInfo {
+  padding: 8px;
+  position: fixed;
+  background: white;
+}
+</style>
